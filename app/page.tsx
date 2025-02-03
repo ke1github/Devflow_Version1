7587b770074fb0a6b45897604860b2a153fd5fb8
Kosi Engineers <kosiengineers@gmail.com>
2025-02-03 21:38:37 +0530

    Setup ESLint

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1 className='text-3xl text-violet-700 font-white'>welcome to the World of Next JS</h1>
+    <h1 className='text-3xl text-violet-700 font-black'>welcome to the World of Next JS</h1>
   )
 }
 
